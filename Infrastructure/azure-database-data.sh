ac6f2b48e0761f4e1559305e51e7041fd58714a1
Temirlan-Tazhibayev <79756276+Temirlan-Tazhibayev@users.noreply.github.com>
2024-02-18 02:15:04 +0600

    Changes in main.tf to provide public IP access to all vm

--- Infrastructure/azure-database-data.sh
+++ Infrastructure/azure-database-data.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
-# install docker
+# Установка Docker
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 
-# install gitlab-runner
+# Установка GitLab Runner
 curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
 dpkg -i gitlab-runner_amd64.deb
 sudo usermod -aG docker gitlab-runner
@@ -17,10 +16,7 @@ sudo systemctl restart gitlab-runner.service
 sudo gitlab-runner register \
     --non-interactive \
     --url "https://gitlab.com/" \
-    --registration-token "TODO_PASTE_TOKEN" \
+    --registration-token "GR13489419kd3bZ3sEKAwmoFjgX9B" \
     --tag-list "database-vm,shell,azure" \
     --description "codechecker" \
     --executor "shell"
-
-# HERE TODO HOME TASK
-# Write database installation script here
--- Infrastructure/azure-docker-data.sh
+++ Infrastructure/azure-docker-data.sh
@@ -4,8 +4,7 @@
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 
@@ -17,7 +16,17 @@ sudo systemctl restart gitlab-runner.service
 sudo gitlab-runner register \
     --non-interactive \
     --url "https://gitlab.com/" \
-    --registration-token "TODO_PASTE_TOKEN" \
+    --registration-token "GR13489419kd3bZ3sEKAwmoFjgX9B" \
     --tag-list "docker-vm,shell,azure" \
     --description "codechecker" \
     --executor "shell"
+
+# docker-image контейнер подготовленный для предварительной компиляции проекта (project builder inside docker)
+# gitlab-runner register \
+#     --non-interactive \
+#     --url "https://gitlab.com/" \
+#     --registration-token "GR13489419kd3bZ3sEKAwmoFjgX9B" \
+#     --tag-list "docker-vm,shell,azure" \
+#     --description "codechecker" \
+#     --executor "shell" \
+#     --docker-image: "" \
