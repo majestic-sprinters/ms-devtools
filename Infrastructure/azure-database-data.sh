aea2a019763dea29de99050c9cbcbdc188e727da
khanZhomart <jtalgatulu@yandex.kz>
2024-02-18 15:26:09 +0600

    Fix initial shell scripts, update compose file for mongodb deployment, include infrastructure compose file (stack)

--- Infrastructure/azure-database-data.sh
+++ Infrastructure/azure-database-data.sh
@@ -7,20 +7,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-
-# install gitlab-runner
-curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
-dpkg -i gitlab-runner_amd64.deb
-sudo usermod -aG docker gitlab-runner
-sudo systemctl restart gitlab-runner.service
-sudo gitlab-runner register \
-    --non-interactive \
-    --url "https://gitlab.com/" \
-    --registration-token "TODO_PASTE_TOKEN" \
-    --tag-list "database-vm,shell,azure" \
-    --description "codechecker" \
-    --executor "shell"
-
-# HERE TODO HOME TASK
-# Write database installation script here
+apt-get install -y docker-ce docker-ce-cli containerd.io docker.io docker-compose
